68dc8b63f105eb56a86c0b558a05ef71e0258ab1
Gy <gouy2@bupt.edu.cn>
2023-10-26 15:19:12 +0800

    fkall

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/assets/css/global.css'
+
+Vue.use(ElementUI, {size : small});
 
 Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
